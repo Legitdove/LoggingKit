4197fba51560d7297a6c16cee0b64dc7ea9192ce
Legitdove <67566592+Legitdove@users.noreply.github.com>
2024-06-29 19:27:48 +0100

    Initial Commit

---
+++ LoggingKit/LoggingKit.h
@@ -0,0 +1,18 @@
+//
+//  LoggingKit.h
+//  LoggingKit
+//
+//  Created by Jacob Brewer on 6/29/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoggingKit.
+FOUNDATION_EXPORT double LoggingKitVersionNumber;
+
+//! Project version string for LoggingKit.
+FOUNDATION_EXPORT const unsigned char LoggingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggingKit/PublicHeader.h>
+
+
---
+++ LoggingKitTests/LoggingKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoggingKitTests.swift
+//  LoggingKitTests
+//
+//  Created by Jacob Brewer on 6/29/24.
+//
+
+import Testing
+@testable import LoggingKit
+
+struct LoggingKitTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
